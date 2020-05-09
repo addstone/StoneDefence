18443648a7c7e62bdb25bd28d58fad6e54775c6f
Smartuil <563412673@qq.com>
2020-05-09 16:41:14 +0800

    核心架构分析

---
+++ Source/StoneDefence/Core/GameCore/TowersDefenceGameUserSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "TowersDefenceGameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STONEDEFENCE_API UTowersDefenceGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+	
+};
