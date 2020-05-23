91d836aca7b391874fbe2bfed73efd261e05bd16
Smartuil <563412673@qq.com>
2020-05-23 18:56:29 +0800

    蒙皮网格渲染数据拷贝到静态网格

--- Source/StoneDefence/StoneDefenceUtils.cpp
+++ Source/StoneDefence/StoneDefenceUtils.cpp
@@ -5,6 +5,13 @@
 #include "Particles/ParticleSystemComponent.h"
 #include "Particles/TypeData/ParticleModuleTypeDataMesh.h"
 #include "RawMesh.h"
+#include "Engine/SkeletalMesh.h"
+#include "SkeletalRenderPublic.h"
+#include "Components/SkinnedMeshComponent.h"
+#include "Rendering/SkeletalMeshRenderData.h"
+#include "Rendering/SkeletalMeshLODRenderData.h"
+#include "Math/UnrealMathSSE.h"
+#include "RawIndexBuffer.h"
 
 ARuleOfTheCharacter * StoneDefenceUtils::FindTargetRecently(const TArray<ARuleOfTheCharacter*> &InCharacters, const FVector &Loc)
 {
@@ -78,27 +85,130 @@ UStaticMesh * MeshUtils::ParticleSystemCompnentToStaticMesh(UParticleSystemCompo
 	return NewStaticMesh;
 }
 
+struct FMeshTracker
+{
+	FMeshTracker()
+		:bValidColors(false)
+	{
+		FMemory::Memset(bValidTexCoords, 0);
+	}
+
+	bool bValidTexCoords[MAX_MESH_TEXTURE_COORDS];
+	bool bValidColors;
+};
+
+void SkeletalMeshToRawMeshes(
+	USkeletalMeshComponent* InComponent,
+	int32 InOverallMaxLODs,
+	const FMatrix& InComponentToWorld,
+	FMeshTracker &MeshTracker,
+	FRawMesh &RawMesh)
+{
+	FSkeletalMeshLODInfo& SrcLODInfo = *(InComponent->SkeletalMesh->GetLODInfo(InOverallMaxLODs));
+
+	TArray<FFinalSkinVertex> FinalVertices;
+	InComponent->GetCPUSkinnedVertices(FinalVertices, InOverallMaxLODs);
+	FSkeletalMeshRenderData& SkeletalMeshRenderData = InComponent->MeshObject->GetSkeletalMeshRenderData();
+	FSkeletalMeshLODRenderData& LODData = SkeletalMeshRenderData.LODRenderData[InOverallMaxLODs];
+
+	for (int32 VertIndex = 0; VertIndex < FinalVertices.Num(); ++VertIndex)
+	{
+		RawMesh.VertexPositions.Add(InComponentToWorld.TransformPosition(FinalVertices[VertIndex].Position));
+	}
+
+	const uint32 NumTexCoords = FMath::Min(LODData.StaticVertexBuffers.StaticMeshVertexBuffer.GetNumTexCoords(), (uint32)MAX_MESH_TEXTURE_COORDS);
+	const int32 NumSections = LODData.RenderSections.Num();
+
+	FRawStaticIndexBuffer16or32Interface& IndexBuffer = *LODData.MultiSizeIndexContainer.GetIndexBuffer();
+	for (int32 SectionIndex = 0; SectionIndex < NumSections; SectionIndex++)
+	{
+		const FSkelMeshRenderSection& SkelMeshSection = LODData.RenderSections[SectionIndex];
+		if (InComponent->IsMaterialSectionShown(SkelMeshSection.MaterialIndex, InOverallMaxLODs))
+		{
+			const int32 NumWedges = SkelMeshSection.NumTriangles * 3;
+			for (int32 WedgeIndex = 0; WedgeIndex < NumWedges; WedgeIndex++)
+			{
+				const int32 VertexIndexForWedge = IndexBuffer.Get(SkelMeshSection.BaseIndex + WedgeIndex);
+				RawMesh.WedgeIndices.Add(VertexIndexForWedge);
+
+				const FFinalSkinVertex& SkinnedVertex = FinalVertices[VertexIndexForWedge];
+				const FVector TangentX = InComponentToWorld.TransformVector(SkinnedVertex.TangentX.ToFVector());
+				const FVector TangentZ = InComponentToWorld.TransformVector(SkinnedVertex.TangentZ.ToFVector());
+				const FVector4 UnpackedTangentX = SkinnedVertex.TangentX.ToFVector4();
+				const FVector TangentY = (TangentZ ^ TangentX).GetSafeNormal() * UnpackedTangentX.W;
+
+				RawMesh.WedgeTangentX.Add(TangentX);
+				RawMesh.WedgeTangentY.Add(TangentY);
+				RawMesh.WedgeTangentZ.Add(TangentZ);
+				for (uint32 TexCoordIndex = 0; TexCoordIndex < MAX_MESH_TEXTURE_COORDS; TexCoordIndex++)
+				{
+					if (TexCoordIndex >= NumTexCoords)
+					{
+						RawMesh.WedgeTexCoords[TexCoordIndex].AddDefaulted();
+					}
+					else
+					{
+						RawMesh.WedgeTexCoords[TexCoordIndex].Add(LODData.StaticVertexBuffers.StaticMeshVertexBuffer.GetVertexUV(VertexIndexForWedge, TexCoordIndex));
+						MeshTracker.bValidTexCoords[TexCoordIndex] = true;
+					}
+				}
+
+				if (LODData.StaticVertexBuffers.ColorVertexBuffer.IsInitialized())
+				{
+					RawMesh.WedgeColors.Add(LODData.StaticVertexBuffers.ColorVertexBuffer.VertexColor(VertexIndexForWedge));
+					MeshTracker.bValidColors = true;
+				}
+				else
+				{
+					RawMesh.WedgeColors.Add(FColor::White);
+				}
+			}
+
+			int32 MaterialIndex = SkelMeshSection.MaterialIndex;
+			if (SrcLODInfo.LODMaterialMap.IsValidIndex(SectionIndex) && SrcLODInfo.LODMaterialMap[SectionIndex] != INDEX_NONE)
+			{
+				MaterialIndex = FMath::Clamp<int32>(SrcLODInfo.LODMaterialMap[SectionIndex], 0, InComponent->SkeletalMesh->Materials.Num());
+			}
+
+			for (uint32 TriIndex = 0; TriIndex < SkelMeshSection.NumTriangles; TriIndex++)
+			{
+				RawMesh.FaceMaterialIndices.Add(MaterialIndex);
+				RawMesh.FaceSmoothingMasks.Add(0);
+			}
+		}
+	}
+}
+
+bool IsValidSkeletalMeshComponent(USkeletalMeshComponent* InComponent)
+{
+	return InComponent && InComponent->MeshObject && InComponent->IsVisible();
+}
+
 UStaticMesh * MeshUtils::SkeletalMeshComponentToStaticMesh(USkeletalMeshComponent *SkeletalMeshComponent)
 {
 	UStaticMesh* StaticMesh = nullptr;
 
-	const FTransform& InRootTransform = FTransform::Identity;
+	
 	FString MeshName = FGuid::NewGuid().ToString();
 	StaticMesh = NewObject<UStaticMesh>(nullptr, *MeshName, RF_Transient);
 	StaticMesh->InitResources();
 
-	TArray<FRawMesh> RawMeshes;
-	struct FMeshTracker
-	{
-		FMeshTracker()
-			:bValidColors(false)
-		{
-			FMemory::Memset(bValidTexCoords, 0);
-		}
 
-		bool bValidTexCoords;
-		bool bValidColors;
-	};
+
+
+	FRawMesh RawMesh;
+	FMeshTracker MeshTracker;
+
+	int32 OverallMaxLODs = 0;
+
+	const FTransform& InRootTransform = FTransform::Identity;
+	FMatrix WorldToRoot = InRootTransform.ToMatrixWithScale().Inverse();
+	FMatrix ComponentToWorld = SkeletalMeshComponent->GetComponentTransform().ToMatrixWithScale() * WorldToRoot;
+
+	if (IsValidSkeletalMeshComponent(SkeletalMeshComponent))
+	{
+		SkeletalMeshToRawMeshes(SkeletalMeshComponent, OverallMaxLODs, ComponentToWorld, MeshTracker, RawMesh);
+	}
 
 
 	return StaticMesh;
