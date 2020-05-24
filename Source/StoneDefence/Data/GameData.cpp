8778317b075394dce664bf95d64aa056d0630d32
Smartuil <563412673@qq.com>
2020-05-24 17:04:43 +0800

    玩家数据和游戏场景数据

--- Source/StoneDefence/Data/GameData.cpp
+++ Source/StoneDefence/Data/GameData.cpp
@@ -1,1 +1,28 @@
 #include "GameData.h"
+
+FGameInstanceDatas::FGameInstanceDatas()
+{
+	Init();
+}
+
+void FGameInstanceDatas::Init()
+{
+	NumberOfMonster = 0;
+	GameDifficulty = 0;
+	bAllMainTowerDie = false;
+	bGameOver =false;
+	bCurrentLevelMissionSuccess = false;
+	SpawnMonsterStage = 0;
+	CurrentLevel = INDEX_NONE;
+	TimeInterval = 0.5f;
+	MaxStagesAreMonsters = 0;
+	CurrentStageAreMonsters = 0;
+	GameCount = 0;
+	MaxGameCount = 0;
+	GoldGrowthTime = 1.f;
+	GoldGrowthMaxTime = 1.5f;
+	KillMonstersNumber = 0;
+	KillMonsterBossNumber = 0;
+	TowersDeathNumber = 0;
+	MainTowersDeathNumber = 0;
+}
---
+++ Source/StoneDefence/Data/PlayerData.h
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerData.generated.h"
+
+
+USTRUCT()
+struct FPlayerSkillData
+{
+	GENERATED_BODY()
+
+	FPlayerSkillData();
+
+	void Init();
+
+	bool IsValid();
+
+	UPROPERTY(SaveGame)
+		int32 SkillID;
+
+	UPROPERTY(SaveGame)
+		float SkillCD;
+
+	UPROPERTY(SaveGame)
+		float MaxSkillCD;
+
+	UPROPERTY(SaveGame)
+		int32 SkillNumber;
+};
+
+
+USTRUCT()
+struct FPlayerData
+{
+	GENERATED_USTRUCT_BODY()
+
+	FPlayerData();
+
+	void Init();
+
+	bool IsValid();
+
+	//玩家ID
+	UPROPERTY(SaveGame)
+		int32 PlayID;
+
+	//玩家名字
+	UPROPERTY(SaveGame)
+		FName PlayName;
+
+	//队伍
+	//UPROPERTY(SaveGame)
+	//	TEnumAsByte<ETeam> Team;
+
+	//玩家账户
+	UPROPERTY(SaveGame)
+		FString Account;
+
+	//游戏金币
+	UPROPERTY(SaveGame)
+		int32 GameGold;
+
+	//钻石 需要购买
+	UPROPERTY(SaveGame)
+		int32 Diamonds;
+
+	//铜币 
+	UPROPERTY(SaveGame)
+		int32 Copper;
+
+	UPROPERTY(SaveGame)
+		float GameGoldTime;
+
+	UPROPERTY(SaveGame)
+		float MaxGameGoldTime;
+
+	//玩家拥有的技能
+	UPROPERTY(SaveGame)
+		TArray<int32> SkillIDs;
+
+	//玩家游戏中数据状态
+	UPROPERTY(SaveGame)
+		TArray<FPlayerSkillData> SkillData;
+};
