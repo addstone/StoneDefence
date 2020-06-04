760b21804fb53c4ea3f49eb6bc66e1b130189361
Smartuil <563412673@qq.com>
2020-06-04 14:27:11 +0800

    实现背景音乐和UI音乐功能设置

--- Plugins/SimpleGameSteeings/Source/SimpleGameSteeings/Private/SimpleGameStettings.cpp
+++ Plugins/SimpleGameSteeings/Source/SimpleGameSteeings/Private/SimpleGameStettings.cpp
@@ -1,12 +1,18 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "SimpleGameSettings.h"
+#include "SlateSound/SimpleSlateSound.h"
+#include "Framework/Application/SlateApplication.h"
 
 #define LOCTEXT_NAMESPACE "FSimpleGameSettingsModule"
 
 void FSimpleGameSettingsModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	
+	//◊¢≤·Œ“√«µƒ…˘“Ù ->UI…˘“Ù
+	FSlateApplication& CurrentSlateApp = FSlateApplication::Get();
+	CurrentSlateApp.InitializeSound(TSharedRef<FSimpleSlateSoundDevice>(new FSimpleSlateSoundDevice()));
 }
 
 void FSimpleGameSettingsModule::ShutdownModule()
